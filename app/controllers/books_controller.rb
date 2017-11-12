dfc1d2ae3d69b8839b37139a622d9b5ecf933fd4
Demi Lee <demilee.to@gmail.com>
2017-11-12 17:11:40 -0500

    add respond_to block to accept get req for text, csv, and json for books#index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,22 @@
 class BooksController < ApplicationController
 
+def index
+
+  respond_to do |format|
+    format.html
+    format.text
+    format.csv do
+      csv = Book.generate_csv(Book.all)
+      render plain: csv
+    end
+    format.json do
+      json = []
+      Book.all.each do |book|
+        json << {title: book.title, author: book.author}
+      end
+      render json: json
+    end
+  end
+end
 
 end
